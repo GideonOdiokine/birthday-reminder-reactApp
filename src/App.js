04acb7f7adc8c54b3696224ffb248b563e598836
Gidysmart Odiokine <gidysmartodiokineh@gmail.com>
2021-05-03 13:37:53 +0100

    added a clear btn functionality

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ const [people, setPeople] = useState(data)
   return (
   <main>
     <section className='container'>
-       <h3>0 birthdays today</h3>
-       <List />
-       <button onClick={()=>console.log("You click me")}>Clear All</button>
+       <h3>{people.length} birthdays today</h3>
+       <List people={people} />
+       <button onClick={()=>setPeople([])}>Clear All</button>
     </section>
     </main>
     );
